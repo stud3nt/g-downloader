5521e1ea7824a273b4529b3651e9bcaf09d429a2
Andrzej Sokolnicki <andrzej.sokolnicki@iandb.pl>
2020-03-31 23:22:30 +0200

    E2_FN2 - fixes

--- src/Utils/DateTimeHelper.php
+++ src/Utils/DateTimeHelper.php
@@ -13,32 +13,32 @@ class DateTimeHelper
         $differenceText = '';
 
         if ($difference->y > 0)
-            $differenceText .= (empty($text) ? '' : ' ').$difference->y.' year'.(($difference->y > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->y.' year'.(($difference->y > 1) ? 's' : '');
 
         if ($difference->m > 0) {
-            $differenceText .= (empty($text) ? '' : ' ').$difference->m.' month'.(($difference->m > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->m.' month'.(($difference->m > 1) ? 's' : '');
             return $differenceText;
         }
 
         if ($difference->d > 0) {
-            $differenceText .= (empty($text) ? '' : ' ').$difference->d.' day'.(($difference->d > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->d.' day'.(($difference->d > 1) ? 's' : '');
             return $differenceText;
         }
 
         if ($difference->h > 0) {
-            $differenceText .= (empty($text) ? '' : ' ').$difference->h.' hour'.(($difference->h > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->h.' hour'.(($difference->h > 1) ? 's' : '');
             return $differenceText;
         }
 
         if ($difference->i > 0) {
-            $differenceText .= (empty($text) ? '' : ' ').$difference->i.' minute'.(($difference->i > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->i.' minute'.(($difference->i > 1) ? 's' : '');
 
             if ($difference->i > 2)
                 return $differenceText;
         }
 
         if ($difference->s > 0)
-            $differenceText .= (empty($text) ? '' : ' ').$difference->s.' second'.(($difference->s > 1) ? 's' : '');
+            $differenceText .= (empty($differenceText) ? '' : ' ').$difference->s.' second'.(($difference->s > 1) ? 's' : '');
 
         return $differenceText;
     }
